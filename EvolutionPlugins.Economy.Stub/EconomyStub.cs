f7bb830ff3836bbd538ba6f38c62069355ff80ac
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-03-01 02:34:08 +0700

    Initial commit

---
+++ EvolutionPlugins.Economy.Stub/EconomyStub.cs
@@ -0,0 +1,16 @@
+﻿using OpenMod.API.Plugins;
+using OpenMod.Core.Plugins;
+using System;
+
+[assembly: PluginMetadata("EvolutionPlugins.Economy.Stub", Author = "EvolutionPlugins", DisplayName = "Economy Stub",
+    Website = "https://discord.gg/5MT2yke")]
+
+namespace EvolutionPlugins.Economy.Stub
+{
+    public class EconomyStub : OpenModUniversalPlugin
+    {
+        public EconomyStub(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
---
+++ EvolutionPlugins.Economy.Stub/Services/EconomyProviderStub.cs
@@ -0,0 +1,33 @@
+﻿using OpenMod.API.Ioc;
+using OpenMod.API.Prioritization;
+using OpenMod.Extensions.Economy.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace EvolutionPlugins.Economy.Stub.Services
+{
+    [PluginServiceImplementation(Priority = Priority.Lowest)]
+    public class EconomyProviderStub : IEconomyProvider
+    {
+        private readonly NotImplementedException m_Exception = new NotImplementedException("Install \"OpenMod.Economy\" plugin to enable Economy manipulation");
+
+        public string CurrencyName => throw m_Exception;
+
+        public string CurrencySymbol => throw m_Exception;
+
+        public Task<decimal> GetBalanceAsync(string ownerId, string ownerType)
+        {
+            throw m_Exception;
+        }
+
+        public Task SetBalanceAsync(string ownerId, string ownerType, decimal balance)
+        {
+            throw m_Exception;
+        }
+
+        public Task<decimal> UpdateBalanceAsync(string ownerId, string ownerType, decimal changeAmount, string reason)
+        {
+            throw m_Exception;
+        }
+    }
+}
